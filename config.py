f99bfa6aec2207af64eabda096748d2ad3afd42f
aylinforbes <139925798+aylinforbes@users.noreply.github.com>
2024-05-06 18:22:20 -0700

    first commit

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ class Config():
   Otherwise create the config variable if not done already
   '''
   FLASK_APP = os.getenv('FLASK_APP') 
-  FLASK_DEBUG = os.environ.get('FLASK_DEBUG') 
+  #FLASK_DEBUG = os.environ.get('FLASK_DEBUG') 
   FLASK_ENV = os.environ.get('FLASK_ENV') 
   SECRET_KEY = os.environ.get('SECRET_KEY') or 'Ryan will never get access to my CSS' 
   SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or 'sqlite:///' + os.path.join(basedir, 'app.db')
